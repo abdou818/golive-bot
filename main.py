d4e6a87c84283e282e7180b479238985cfa06007
abdou818 <nothing11223345@gmail.com>
2025-07-10 00:02:34 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import time
+
+with open('config.json') as f:
+    config = json.load(f)
+
+headers = {
+    'Authorization': config['token'],
+    'Content-Type': 'application/json'
+}
+
+def go_live():
+    payload = {
+        'type': 1,
+        'guild_id': config['guild_id'],
+        'channel_id': config['channel_id'],
+        'message': config['message']
+    }
+
+    while True:
+        requests.post(
+            'https://discord.com/api/v9/channels/' + config['channel_id'] + '/call',
+            headers=headers,
+            data=json.dumps(payload)
+        )
+        print("Go Live Sent")
+        time.sleep(60 * 5)  # Repeat every 5 minutes
+
+go_live()
