4dff6ee1a8b94a6f0e416a5e991873b43f7a5732
abdou818 <nothing11223345@gmail.com>
2025-07-10 08:08:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ intents.guilds = True
 
 client = discord.Client(intents=intents)
 
-TOKEN = os.environ.get("MTMzNjg3MDA4MzM1MTQxNjgzMg.GVuTZ0.ASc-h7VAatch6oRUIpwGWJcrqPyssT_yb35b7Y")  # أو تقدر تحطه مباشرة كـ string
-VOICE_CHANNEL_ID = int(os.environ.get("1387106900390449203"))  # أو ID الروم الصوتي
+TOKEN = os.environ.get("MTMzNjg3MDA4MzM1MTQxNjgzMg.GVuTZ0.ASc-h7VAatch6oRUIpwGWJcrqPyssT_yb35b7Y")
+VOICE_CHANNEL_ID = int(os.environ.get("1387106900390449203"))
 
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
 
-    # تلقى السيرفر الأول
     for guild in client.guilds:
         channel = guild.get_channel(VOICE_CHANNEL_ID)
         if channel and isinstance(channel, discord.VoiceChannel):
+            # يدخل الروم الصوتي فقط
             await channel.connect()
             print(f"Connected to voice channel: {channel.name}")
             break
 
 client.run(TOKEN)
+
